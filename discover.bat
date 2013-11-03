5d7297d6ea5448d63ab754fc4c481a40124acfef
Sarrailh Rémi <maditnerd@gmail.com>
2013-11-03 23:00:13 +0100

    Silent ping
    one command to rule them all

--- discover.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title "Discovering Network ... Please Wait !!!"
-for /f "tokens=16" %%a in ('ipconfig ^|findstr "IPv4"') do for /f "tokens=1,2,3 delims=." %%a in ('echo %%a') do FOR /L %%x in (1,1,255) do start /B ping -n 1 -w 30 %%a.%%b.%%c.%%x.
-for /f "tokens=14" %%a in ('ipconfig ^|findstr "IPv4"') do for /f "tokens=1,2,3 delims=." %%a in ('echo %%a') do FOR /L %%x in (1,1,255) do start /B ping -n 1 -w 30 %%a.%%b.%%c.%%x.
-
-
--- showrasp.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Searching for Raspberry Pi -----------------------------------
-for /f "tokens=1" %%a in ('arp -a ^| findstr "b8-27"') do echo %%a
-echo --------------------------------------------------------------
-pause
