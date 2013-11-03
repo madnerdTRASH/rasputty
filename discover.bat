f552b65c8c335dfdab9e53fc541e0ccc5e0ff632
Sarrailh Rémi <sarrailh.remi@gmail.com>
2013-11-03 21:20:00 +0100

    Update discover.bat

--- discover.bat
+++ discover.bat
@@ -1,3 +1,5 @@
 @echo off
-echo Discovering network
-FOR /L %%x in (1,1,255) do start ping -n 1 -w 30 192.168.0.%%x
+title "Discovering Network ... Please Wait !!!"
+for /f "tokens=16" %%a in ('ipconfig ^|findstr "IPv4"') do for /f "tokens=1,2,3 delims=." %%a in ('echo %%a') do FOR /L %%x in (1,1,255) do start /B ping -n 1 -w 30 %%a.%%b.%%c.%%x.
+
+
