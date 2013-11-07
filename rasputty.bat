2b182562e8d0639efef1312f2600175b3cafc1b5
Sarrailh Rémi <maditnerd@gmail.com>
2013-11-07 05:43:16 +0100

    Change first for / for better search
    It should now work safely with any language

--- rasputty.bat
+++ rasputty.bat
@@ -1,8 +1,7 @@
 @echo off
 title Discovering Network ... Please Wait !!!
 echo Discovering Network ... Please Wait!!!
-for /f "tokens=16" %%a in ('ipconfig ^|findstr "IPv4"') do for /f "tokens=1,2,3 delims=." %%a in ('echo %%a') do FOR /L %%x in (1,1,255) do start /B ping -n 1 -w 30 %%a.%%b.%%c.%%x. >nul
-for /f "tokens=14" %%a in ('ipconfig ^|findstr "IPv4"') do for /f "tokens=1,2,3 delims=." %%a in ('echo %%a') do FOR /L %%x in (1,1,255) do start /B ping -n 1 -w 30 %%a.%%b.%%c.%%x. >nul
+for /f "tokens=2 delims=:" %%a in ('ipconfig ^|findstr "IPv4"') do for /f "tokens=1,2,3 delims=." %%a in ('echo %%a') do for /L %%x in (1,1,254) do start /B ping -n 1 -w 30 %%a.%%b.%%c.%%x. >nul
 title Search finish!
 echo Raspberry Pi detected -----------------------------------
 for /f "tokens=1" %%a in ('arp -a ^| findstr "b8-27"') do echo %%a
